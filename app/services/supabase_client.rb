04fd64d82bdfaaf62b7f0feda822bf15016003dd
ManaMel <m.y.14ob10@gmail.com>
2025-06-22 14:59:15 +0900

    lintチェックエラー修正

--- app/services/supabase_client.rb
+++ app/services/supabase_client.rb
@@ -1,10 +1,10 @@
-require 'net/http'
-require 'uri'
-require 'json'
+require "net/http"
+require "uri"
+require "json"
 
 class SupabaseClient
-  API_URL = ENV['SUPABASE_API_URL']
-  SERVICE_ROLE_KEY = ENV['SUPABASE_SERVICE_ROLE_KEY']
+  API_URL = ENV["SUPABASE_API_URL"]
+  SERVICE_ROLE_KEY = ENV["SUPABASE_SERVICE_ROLE_KEY"]
 
   def initialize
     @uri = URI(API_URL)
@@ -13,9 +13,9 @@ class SupabaseClient
   def get_users
     url = URI("#{API_URL}/rest/v1/users")
     req = Net::HTTP::Get.new(url)
-    req['apikey'] = SERVICE_ROLE_KEY
-    req['Authorization'] = "Bearer #{SERVICE_ROLE_KEY}"
-    req['Content-Type'] = 'application/json'
+    req["apikey"] = SERVICE_ROLE_KEY
+    req["Authorization"] = "Bearer #{SERVICE_ROLE_KEY}"
+    req["Content-Type"] = "application/json"
 
     res = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
       http.request(req)
@@ -24,13 +24,12 @@ class SupabaseClient
     JSON.parse(res.body)
   end
 
-  
   def create_user(user_params)
     url = URI("#{API_URL}/rest/v1/users")
     req = Net::HTTP::Post.new(url)
-    req['apikey'] = SERVICE_ROLE_KEY
-    req['Authorization'] = "Bearer #{SERVICE_ROLE_KEY}"
-    req['Content-Type'] = 'application/json'
+    req["apikey"] = SERVICE_ROLE_KEY
+    req["Authorization"] = "Bearer #{SERVICE_ROLE_KEY}"
+    req["Content-Type"] = "application/json"
     req.body = user_params.to_json
 
     res = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   }
   get "home/index"
 
-  resources :users, only: [:index]
+  resources :users, only: [ :index ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
