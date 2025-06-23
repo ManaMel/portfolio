e44ad5a6a14f007500d961ade420a8703aa400d2
ManaMel <m.y.14ob10@gmail.com>
2025-06-24 01:05:06 +0900

    録音機能修正

--- app/javascript/recording.js
+++ app/javascript/recording.js
@@ -1,57 +1,51 @@
 'use strict';
-const record = document.querySelector("#buttonRecord");
-const stop = document.querySelector("#buttonStop");
-const audio = document.querySelector("#player");
+ const record = document.querySelector("#buttonRecord");
+ const stop = document.querySelector("#buttonStop");
+ const audio = document.querySelector("#player");
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  console.log("getUserMedia supported.");
-  navigator.mediaDevices
-    .getUserMedia(
-      // 制約 - 音声のみがこのアプリでは必要
-      {
-        audio: true,
-      },
-    )
+ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+   console.log("getUserMedia supported.");
+   navigator.mediaDevices
+     .getUserMedia(
+       {
+         audio: true,
+       }
+     )
+     .then((stream) => {
+       console.log('getUserMediaはサポートされています');
+       const mediaRecorder = new MediaRecorder(stream);
+       let chunks = [];
 
-    // 成功コールバック
-    .then((stream) => {
-      console.log('getUserMediaはサポートされています')
-      const mediaRecorder = new MediaRecorder(stream);
-      record.onclick = () => {
-        mediaRecorder.start();
-        console.log(mediaRecorder.state);
-        console.log("recorder started");
-        record.setAttribute('disabled', '');
-        stop.removeAttribute('disabled');        
-      };
+       record.onclick = () => {
+         mediaRecorder.start();
+         console.log(mediaRecorder.state);
+         console.log("recorder started");
+         record.setAttribute('disabled', '');
+         stop.removeAttribute('disabled');
+       };
 
-      let chunks = [];
+       mediaRecorder.ondataavailable = (e) => {
+         chunks.push(e.data);
+       };
 
-      mediaRecorder.ondataavailable = (e) => {
-        chunks.push(e.data);
-      };
+       stop.onclick = () => {
+         mediaRecorder.stop();
+         console.log(mediaRecorder.state);
+         console.log("recorder stopped");
+         record.removeAttribute('disabled');
+         stop.setAttribute('disabled', '');
+       };
 
-      stop.onclick = () => {
-        mediaRecorder.stop();
-        console.log(mediaRecorder.state);
-        console.log("recorder stopped");
-        record.removeAttribute('disabled');
-        stop.setAttribute('disabled', '');
-      };
-
-      mediaRecorder.onstop = (e) => {
-        const blob = new Blob(chunks, { type: "audio/webm; codecs=opus" });
-        chunks = [];
-        const audioURL = window.URL.createObjectURL(blob);
-        audio.src = audioURL;
-      };
-
+       mediaRecorder.onstop = (e) => {
+         const blob = new Blob(chunks, { type: "audio/webm; codecs=opus" });
+         chunks = [];
+         const audioURL = window.URL.createObjectURL(blob);
+         audio.src = audioURL;
+       };
     })
-
-    // エラーコールバック
-    .catch((err) => {
-      console.error(`The following getUserMedia error occurred: ${err}`);
-    });
-} else {
-  console.log("getUserMedia not supported on your browser!");
-}
+     .catch((err) => {
+       console.error(`The following getUserMedia error occurred: ${err}`);
+     });
+ } else {
+   console.log("getUserMedia not supported on your browser!");
+ }
