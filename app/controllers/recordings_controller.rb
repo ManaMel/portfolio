b9310a4f56b2c59668b9fe287b23c7e08b62d208
ManaMel <m.y.14ob10@gmail.com>
2025-06-28 23:41:52 +0900

    YouTube動画を動的に埋め込む機能を実装

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,4 +1,5 @@
 class RecordingsController < ApplicationController
   def index
+    @videos = Video.all
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def find_youtube_id(video_url)
+    if video_url =~ /v=([\w-]{11})/
+      Regexp.last_match(1)
+    elsif video_url =~ /youtu\.be\/([\w-]{11})/
+      Regexp.last_match(1)
+    elsif video_url =~ /embed\/([\w-]{11})/
+      Regexp.last_match(1)
+    else
+      nil
+    end
+  end
 end
