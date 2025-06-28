f4cdfcb6cb945999516eb1512f1263bd05f09e16
ManaMel <m.y.14ob10@gmail.com>
2025-06-28 23:45:19 +0900

    lintチェック修正

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,7 +9,7 @@ class VideosController < ApplicationController
   def create
     @video = current_user.videos.new(video_params)
     if @video.save
-      redirect_to recordings_path, notice: '伴奏動画を選択しました'
+      redirect_to recordings_path, notice: "伴奏動画を選択しました"
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,7 +18,7 @@ class VideosController < ApplicationController
   def destroy
     video = current_user.videos.find(params[:id])
     video.destroy!
-    redirect_to recordings_path, success: '伴奏動画を削除しました', status: :see_other
+    redirect_to recordings_path, success: "伴奏動画を削除しました", status: :see_other
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :videos, dependent: :destroy
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
