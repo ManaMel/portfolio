7a024fe0800ecfb9660b340bc978eb0b29ccc522
ManaMel <m.y.14ob10@gmail.com>
2025-07-05 02:40:38 +0900

    YouTubeDataAPIで動画検索・埋め込み

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,34 +1,32 @@
 class VideosController < ApplicationController
   def index
-  end
-
-  def new
     @video = Video.new
   end
 
   def create
     @video = current_user.videos.new(video_params)
     if @video.save
-      redirect_to recordings_path, notice: "伴奏動画を選択しました"
+      redirect_to recordings_path, notice: "動画を選択しました"
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   def destroy
     video = current_user.videos.find(params[:id])
     video.destroy!
-    redirect_to recordings_path, success: "伴奏動画を削除しました", status: :see_other
+    redirect_to recordings_path, success: "動画を削除しました", status: :see_other
   end
 
   def search
     keyword = params[:keyword]
     @results = YoutubeSearchServise.new(keyword).search
+    @video = Video.new 
     render :index
   end
 
   private
   def video_params
-    params.require(:video).permit(:video_url)
+    params.require(:video).permit(:video_url, :title, :channel)
   end
 end
