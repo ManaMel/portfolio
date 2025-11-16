3b864a9af0973f28d9fb1a134e8d7b1c08e9b70d
ManaMel <m.y.14ob10@gmail.com>
2025-11-17 01:23:33 +0900

    デプロイエラー修正

--- config/initializers/create_admin_user.rb
+++ config/initializers/create_admin_user.rb
@@ -1,16 +1,21 @@
-if User.where(admin: true).none?
-  admin_email = ENV["ADMIN_EMAIL"]
-  admin_password = ENV["ADMIN_PASSWORD"]
+Rails.application.config.after_initialize do
+  # 本番環境だけで実行
+  next unless Rails.env.production?
 
-  if admin_email.present? && admin_password.present?
-    User.create!(
-      email: admin_email,
-      password: admin_password,
-      password_confirmation: admin_password,
-      admin: true
-    )
-    Rails.logger.info "Admin user created automatically."
-  else
-    Rails.logger.warn "Admin user NOT created. ENV variables missing."
+  admin_email = ENV['ADMIN_EMAIL']
+  admin_password = ENV['ADMIN_PASSWORD']
+
+  if User.where(admin: true).none?
+    if admin_email.present? && admin_password.present?
+      User.create!(
+        email: admin_email,
+        password: admin_password,
+        password_confirmation: admin_password,
+        admin: true
+      )
+      Rails.logger.info "[Admin] Admin user created automatically."
+    else
+      Rails.logger.warn "[Admin] Admin user NOT created. ENV variables missing."
+    end
   end
 end
