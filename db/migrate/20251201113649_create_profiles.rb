133681549c60bc1ff782e8419d65688bf267f8cc
ManaMel <m.y.14ob10@gmail.com>
2025-12-01 20:40:51 +0900

    migrationファイル修正

--- db/migrate/20251201113649_create_profiles.rb
+++ db/migrate/20251201113649_create_profiles.rb
@@ -1,6 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration[7.2]
   def change
-    create_table :profiles do |t|
+    create_table :profiles, if_not_exists: true do |t|
       t.references :user, null: false, foreign_key: true
       t.string :name
       t.text :musical_carrer
